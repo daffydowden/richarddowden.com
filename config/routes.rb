00fe28ecca4c76d7ec46f09f7b5186b0b0232e52
Richard Dowden <richard.dowden@purpletonic.com>
2011-02-01 18:50:07 +0000

    Created routes for pages and portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 RicharddowdenCom::Application.routes.draw do
+  get "portfolio/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-	match 'welcome' => 'welcome#index'
+	match 'index.html' => 'page#index'
+  match 'about.html' => 'page#about'
+  match 'blog.html' => 'page#blog'
+  match 'contact.html' => 'page#contact'
+
+  match 'portfolio.html' => 'portfolio#index'
+
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
@@ -49,7 +57,7 @@ RicharddowdenCom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
+  root :to => "page#index"
 
   # See how all your routes lay out with "rake routes"
 
